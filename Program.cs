0517c16907e722c1ae456a7f6c27c3f104bca2b8
Patricio Morales <78184534+Wargish@users.noreply.github.com>
2024-06-02 17:24:17 -0400

    Connect Firebase

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Cyber_Electro;
 
+string credentialPath = "@C:/Users/Patricio/RiderProjects/cyber-electro-d346e-firebase-adminsdk-lpc79-ad0c6d5066";
+Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialPath);
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
