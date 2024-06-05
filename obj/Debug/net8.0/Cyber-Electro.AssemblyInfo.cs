0d1baff811ce18ad504575a899185691a06d7e61
Patricio Morales <78184534+Wargish@users.noreply.github.com>
2024-06-05 15:06:01 -0400

    Orden de Compra actualizada y con funciones de calculo

--- obj/Debug/net8.0/Cyber-Electro.AssemblyInfo.cs
+++ obj/Debug/net8.0/Cyber-Electro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cyber-Electro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0517c16907e722c1ae456a7f6c27c3f104bca2b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e1689d707ff6f8871910b9c188603df165e19b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cyber-Electro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cyber-Electro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
